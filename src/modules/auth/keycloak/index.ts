c464d008288f6257c5005ed38d50f2b2de1ada9e
Philip <philiphagemann@icloud.com>
2022-11-05 22:29:25 +0100

    fixed typo

--- src/modules/auth/keycloak/index.ts
+++ src/modules/auth/keycloak/index.ts
@@ -55,7 +55,7 @@ function onTokenExpired() {
     console.log('token expired');
     keycloak.updateToken(30)
         .then(() => {
-            console.log('successfully get a new token', keycloak.token);
+            console.log('successfully got a new token', keycloak.token);
             setStoreValues();
         })
         .catch((error) => {
