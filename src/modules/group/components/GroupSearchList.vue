098267ac218c6ee4b6b5e77f5748912cccaa8bb7
Robert Bagdahn <robertbaggi@gmail.com>
2022-11-05 23:06:12 +0100

    improved some logic

--- src/modules/group/components/GroupSearchList.vue
+++ src/modules/group/components/GroupSearchList.vue
@@ -1,46 +1,53 @@
 <template>
-  <List
-    :name="'Gruppen'"
-    :items="groups"
-    :searchValue="searchValue"
-    :sortOptions="sortOptions"
-    :filters="filters"
-    :buttonList="buttonList"
-    mainPageLink="GroupMain"
-    detailPageLink="GroupOverview"
-    v-if="isAuth"
-  >
-    <template #listitem="{ item }">
-      <div class="flex min-w-0 flex-1 items-center">
-        <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-          <div>
-            <p class="truncate text-sm font-medium text-blue-600">
-              {{ item.name }}
-            </p>
-            <p class="mt-2 flex items-center text-sm text-gray-500">
-              <TagIcon
-                class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                aria-hidden="true"
-              />
-              <span class="truncate">{{ item.majorClass }}</span>
-            </p>
-          </div>
-          <div class="hidden md:block">
+  <div>
+    <List
+      :name="'Gruppen'"
+      :items="groups"
+      :searchValue="searchValue"
+      :sortOptions="sortOptions"
+      :filters="filters"
+      :buttonList="buttonList"
+      mainPageLink="GroupMain"
+      detailPageLink="GroupOverview"
+    >
+      <template #listitem="{ item }">
+        <div class="flex min-w-0 flex-1 items-center">
+          <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
             <div>
-              <p class="mt-1 flex items-center text-sm text-gray-500">
-                <NutriSlim :nutriClass="item.nutriClass" />
+              <p class="truncate text-sm font-medium text-blue-600">
+                {{ item.name }}
+              </p>
+              <p class="mt-2 flex items-center text-sm text-gray-500">
+                <nav class="flex" aria-label="Breadcrumb">
+                  <ol role="list" class="flex items-center space-x-4">
+                    <li class="flex">
+                      <div class="flex items-center">
+                        <a href="#" class="text-gray-400 hover:text-gray-500">
+                          <TagIcon class="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+                          <span class="sr-only">Home</span>
+                        </a>
+                      </div>
+                    </li>
+                    <li v-for="page in collectParents(item.parent)" :key="page.name" class="flex">
+                      <div class="flex items-center">
+                        <ChevronRightIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                        <p class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" >{{ page.name }}</p>
+                      </div>
+                    </li>
+                  </ol>
+                </nav>
               </p>
             </div>
           </div>
         </div>
-      </div>
-    </template>
-  </List>
+      </template>
+    </List>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watch, onMounted, computed } from "vue";
-import { TagIcon } from "@heroicons/vue/20/solid";
+import { TagIcon, ChevronRightIcon, HomeIcon } from "@heroicons/vue/20/solid";
 import List from "@/components/base/list/Main.vue";
 import { useRoute } from "vue-router";
 import { useGroupStore } from "@/modules/group/store/index.ts";
@@ -83,32 +90,28 @@ function updateSearch(params) {
   groupStore.fetchGroupsOverview(params);
 }
 
-const sortOptions = [
-  { name: "A-Z", value: "alpha", current: true },
-  { name: "Neuste", value: "-created_at", current: false },
-  { name: "Gesund", value: "nutri_points", current: false },
-];
+function collectParents(items) {
+  debugger;
+  return flattenObject(items)
+}
 
-const filters = [
-  {
-    id: "nutri_class",
-    name: "Nutri Score",
-    options: [
-      { value: "1", label: "Klasse A", checked: false },
-      { value: "2", label: "Klasse B", checked: false },
-      { value: "3", label: "Klasse C", checked: false },
-      { value: "4", label: "Klasse D", checked: false },
-      { value: "5", label: "Klasse E", checked: false },
-    ],
-  },
-  {
-    id: "physical_viscosity",
-    name: "Festigkeit",
-    options: [
-      { value: "beverage", label: "Flüssig", checked: false },
-      { value: "solid", label: "Fest", checked: false },
-    ],
-  },
-];
-const buttonList = [{ name: "Neue Zutat", linkName: "IngredientCreate" }];
+function flattenObject(items) {
+  const returnArray = []
+  if (items?.parent?.parent?.parent) {
+    returnArray.push(items.parent.parent.parent);
+  }
+  if (items?.parent?.parent) {
+    returnArray.push(items.parent.parent);
+  }
+  if (items?.parent) {
+    returnArray.push(items.parent);
+  }
+
+  return returnArray;
+}
+
+const sortOptions = [];
+
+const filters = [];
+const buttonList = [{ name: "Neue Gruppe", linkName: "" }];
 </script>
