1af2a9583d949508a666ad04e0926bbaabfe6c6d
jomichaelis <42468538+jomichaelis@users.noreply.github.com>
2022-11-22 23:00:28 +0100

    added margin-top and fixed typos

--- src/modules/settings/views/tabs/Notifications.vue
+++ src/modules/settings/views/tabs/Notifications.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    Hier sollst du deine offenen und bearbeiten Anträge stehen könen.
+  <div class="mt-5">
+    Hier sollst du deine offenen und bearbeiteten Anträge sehen können.
   </div>
 </template>
 
