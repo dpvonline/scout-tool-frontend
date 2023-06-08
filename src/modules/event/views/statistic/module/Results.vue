fc57eb550fe95f913d27959585c89c82d46e89cc
Robert Bagdahn <robertbaggi@gmail.com>
2023-06-08 16:02:10 +0200

    improved amount handling in payment module

--- src/modules/event/views/statistic/module/Results.vue
+++ src/modules/event/views/statistic/module/Results.vue
@@ -68,7 +68,7 @@
           class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
         >
           <dt class="truncate text-sm font-medium text-gray-500">
-            Alt-Rover (über 25 Jahre)
+            Alt-Rover (ab 25 Jahre)
           </dt>
           <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
             {{ eventAgeGroupsSummary?.altRover }}
