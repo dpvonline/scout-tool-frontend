36be837e4b74a38fcbab251c038de687f242c453
Robert <robertbaggi@gmail.com>
2023-08-20 15:20:43 +0200

    disbale anmelde button bevore registration start

--- src/components/button/Primary.vue
+++ src/components/button/Primary.vue
@@ -2,10 +2,10 @@
   <button
     @click="onButtonClicked"
     type="button"
-    :disabled="isLoading"
+    :disabled="isLoading || disabled"
     class="inline-flex items-center justify-center rounded-md border border-transparent py-2 px-4 text-xs font-medium text-white shadow-sm"
     :class="
-      isLoading
+      isLoading || disabled
         ? `bg-blue-300 focus:outline-none bg-blue-500`
         : `focus:outline-none focus:ring-2 focus:ring-${color}-500 focus:ring-offset-2 hover:bg-${color}-700 bg-${color}-600`
     "
@@ -18,8 +18,8 @@
     <component
       v-if="props.icon"
       :is="props.icon"
-      class=" h-4 w-4 text-white"
-      :class="props.label !== '' ? '-ml-1 mr-2': ''"
+      class="h-4 w-4 text-white"
+      :class="props.label !== '' ? '-ml-1 mr-2' : ''"
       aria-hidden="true"
     />
     {{ props.label }}
@@ -35,6 +35,7 @@ const props = defineProps({
   isLoading: { type: Boolean, required: false },
   color: { type: String, required: false, default: "blue" },
   icon: { type: Function, required: false, default: null },
+  disabled: { type: Boolean, required: false, default: false },
 });
 
 const emit = defineEmits(["click"]);
