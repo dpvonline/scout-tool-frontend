09783b0eae83651ba390537983f490ac05c685b6
Robert <robertbaggi@gmail.com>
2024-01-13 09:21:02 +0100

    remove dark

--- src/components/list/LoadingItem.vue
+++ src/components/list/LoadingItem.vue
@@ -2,9 +2,9 @@
   <div class="flex justify-center items-center my-12">
     <div class="flex justify-center items-center">
       <div class="flex items-center justify-center space-x-2">
-        <div class="w-6 h-6 rounded-full animate-pulse dark:bg-blue-400"></div>
-        <div class="w-6 h-6 rounded-full animate-pulse dark:bg-blue-400"></div>
-        <div class="w-6 h-6 rounded-full animate-pulse dark:bg-blue-400"></div>
+        <div class="w-6 h-6 rounded-full animate-pulse"></div>
+        <div class="w-6 h-6 rounded-full animate-pulse"></div>
+        <div class="w-6 h-6 rounded-full animate-pulse"></div>
       </div>
     </div>
   </div>
--- src/modules/person/views/NewExcel.vue
+++ src/modules/person/views/NewExcel.vue
@@ -21,12 +21,12 @@
         </p>
         <div v-if="isReadyToUpload">
           <label
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            class="block mb-2 text-sm font-medium text-gray-900"
             for="file_input"
             >Excel Personen-Liste</label
           >
           <input
-            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
             aria-describedby="file_input_help"
             id="file_input"
             type="file"
@@ -34,7 +34,7 @@
             @change="handleFileUpload"
           />
           <p
-            class="mt-1 text-sm text-gray-500 dark:text-gray-300"
+            class="mt-1 text-sm text-gray-500"
             id="file_input_help"
           >
             xlsx-Datei auswählen (max. 150 Zeilen)
