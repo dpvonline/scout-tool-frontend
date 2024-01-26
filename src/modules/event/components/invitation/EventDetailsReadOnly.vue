ccb6eff8404c69d10675a67dfe76d31a621c1e56
Robert <robertbaggi@gmail.com>
2024-01-26 15:42:44 +0100

    improvments for booking types

--- src/modules/event/components/invitation/EventDetailsReadOnly.vue
+++ src/modules/event/components/invitation/EventDetailsReadOnly.vue
@@ -25,7 +25,7 @@
         Alles rund um die Termine
       </p>
     </div>
-    <TimelineEvent :event="event"/>
+    <TimelineEvent :event="event" />
   </div>
   <div class="border-t-8 border-gray-100 px-4 py-5 sm:px-6">
     <div class="pb-3">
@@ -106,7 +106,7 @@
           Veranwortliche Personen
         </dt>
         <dd class="mt-1 text-sm text-gray-900">
-          {{ event.responsiblePersons?.map((a) => `${a}`).join(", ") }}
+          {{ event.responsiblePersons?.map((a) => `${a.username}`).join(", ") }}
         </dd>
       </div>
       <div class="sm:col-span-1">
@@ -118,8 +118,8 @@
       <div class="sm:col-span-1">
         <dt class="text-sm font-medium text-gray-500">Wer ist eingeladen?</dt>
         <dd
-            v-if="event.invitedGroups && event.invitedGroups.length > 0"
-            class="mt-1 text-sm text-gray-900"
+          v-if="event.invitedGroups && event.invitedGroups.length > 0"
+          class="mt-1 text-sm text-gray-900"
         >
           {{ event.invitedGroups?.map((a) => `${a.displayName}`).join(", ") }}
         </dd>
@@ -141,45 +141,45 @@
     <div class="sm:col-span-2" v-if="event.eventmoduleSet?.length">
       <dl class="space-y-6 divide-y divide-gray-900/10">
         <Disclosure
-            v-for="child in event.eventmoduleSet"
-            :key="child.id"
-            v-slot="{ open }"
-            class="pt-2"
-            as="div"
+          v-for="child in event.eventmoduleSet"
+          :key="child.id"
+          v-slot="{ open }"
+          class="pt-2"
+          as="div"
         >
           <dt>
             <DisclosureButton
-                class="flex w-full items-start justify-between text-left text-gray-900"
+              class="flex w-full items-start justify-between text-left text-gray-900"
             >
-                <span class="text-base font-normal leading-7">{{
-                  child.header
-                }}</span>
+              <span class="text-base font-normal leading-7">{{
+                child.header
+              }}</span>
               <span class="ml-6 flex h-7 items-center">
-                  <PlusSmallIcon
-                      v-if="!open"
-                      class="h-6 w-6"
-                      aria-hidden="true"
-                  />
-                  <MinusSmallIcon v-else class="h-6 w-6" aria-hidden="true"/>
-                </span>
+                <PlusSmallIcon
+                  v-if="!open"
+                  class="h-6 w-6"
+                  aria-hidden="true"
+                />
+                <MinusSmallIcon v-else class="h-6 w-6" aria-hidden="true" />
+              </span>
             </DisclosureButton>
           </dt>
           <DisclosurePanel as="dd" class="mt-2 pr-12">
             <p
-                class="text-base leading-7 text-gray-600"
-                v-html="child.description || ''"
+              class="text-base leading-7 text-gray-600"
+              v-html="child.description || ''"
             ></p>
             <div
-                v-for="attribute in child.attributeModules"
-                :key="attribute.id"
-                class="mb-3"
+              v-for="attribute in child.attributeModules"
+              :key="attribute.id"
+              class="mb-3"
             >
               <p class="text-sm leading-7 text-gray-800">
                 {{ attribute.title }}
               </p>
               <p
-                  class="text-sm leading-7 text-gray-500"
-                  v-html="attribute.text"
+                class="text-sm leading-7 text-gray-500"
+                v-html="attribute.text"
               ></p>
             </div>
           </DisclosurePanel>
@@ -201,20 +201,23 @@
     <div class="sm:col-span-2" v-if="event?.bookingOptions?.length">
       <dd class="text-sm text-gray-900">
         <ul
-            role="list"
-            class="divide-y divide-gray-200 rounded-md border border-gray-200"
+          role="list"
+          class="divide-y divide-gray-200 rounded-md border border-gray-200"
         >
           <li
-              class="flex items-center justify-between py-3 pl-3 pr-4 text-sm"
-              v-for="child in event?.bookingOptions"
-              :key="child.id"
+            class="flex items-center justify-between py-3 pl-3 pr-4 text-sm"
+            v-for="child in event?.bookingOptions"
+            :key="child.id"
           >
             <div class="flex w-0 flex-1 items-center">
               <AdjustmentsVerticalIcon
-                  class="h-5 w-5 mr-2 flex-shrink-0 text-gray-400"
-                  aria-hidden="true"
+                class="h-5 w-5 mr-2 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
               />
               <span> {{ `${child.name} - ${child.price} €` }}</span>
+              <span v-if="child.bookableTill" class="text-red-500 ml-3">
+                {{ ` bis ${$dayjs(child.bookableTill).format("ll")}` }}</span
+              >
             </div>
             <div class="ml-4 flex-shrink-0"></div>
           </li>
@@ -224,11 +227,14 @@
   </div>
 </template>
 <script setup lang="ts">
-import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/vue";
-import {AdjustmentsVerticalIcon, MinusSmallIcon, PlusSmallIcon} from "@heroicons/vue/24/outline";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import {
+  AdjustmentsVerticalIcon,
+  MinusSmallIcon,
+  PlusSmallIcon,
+} from "@heroicons/vue/24/outline";
 
 import TimelineEvent from "@/modules/event/components/general/TimelineEvent.vue";
-
 
 const props = defineProps({
   event: Object,
