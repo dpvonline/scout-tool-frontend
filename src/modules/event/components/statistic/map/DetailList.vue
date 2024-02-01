8d0642326edc7b55adaa15cfae2553407ff20aa7
Robert <robertbaggi@gmail.com>
2024-02-01 21:35:34 +0100

    add leafmap

--- src/modules/event/components/statistic/map/DetailList.vue
+++ src/modules/event/components/statistic/map/DetailList.vue
@@ -1,148 +1,82 @@
 <template>
-  <div>
-    <div>
-      <fieldset class="my-4 mx-2">
-        <legend class="sr-only">Notification method</legend>
-        <div
-          class="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0"
-        >
-          <div v-for="type in types" :key="type.id" class="flex items-center">
-            <input
-              :id="type.id"
-              name="notification-method"
-              type="radio"
-              :checked="type.id === picked"
-              :value="type.id"
-              v-model="picked"
-              class="h-4 w-4 border-gray-300 text-blue-600 focus:ring-blue-600"
-            />
-            <label
-              :for="type.id"
-              class="ml-3 block text-sm font-medium leading-6 text-gray-900"
-              >{{ type.title }}</label
-            >
-          </div>
-        </div>
-      </fieldset>
-    </div>
-    <GoogleMap
-      :api-key="GOOGLE_MAPS_API_KEY"
-      style="width: 100%; height: 800px"
-      :center="center"
-      :zoom="7"
-    >
-      <Marker
-        v-for="group in hierarchyMappingDetailed"
-        :key="group.id"
-        :options="getMarkerOptions(group)"
-      >
-        <InfoWindow>
-          <div id="contet">
-            <div id="siteNotice"></div>
-            <h1 id="firstHeading" class="firstHeading">{{ group.name }}</h1>
-            <div id="bodyContent">
-              <p></p>
-            </div>
-          </div>
-        </InfoWindow>
-      </Marker>
-    </GoogleMap>
-  </div>
+  <div id="mapContainer"></div>
 </template>
 
-<script setup>
-const GOOGLE_MAPS_API_KEY =
-  import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY || "no Key";
-import { defineComponent, onMounted } from "vue";
-import { GoogleMap, Marker, InfoWindow } from "vue3-google-map";
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 import { useEventStore } from "@/modules/event/store";
+import { useRoute, useRouter } from "vue-router";
+import { ref, computed } from "vue";
+
+const router = useRouter();
+
 const eventStore = useEventStore();
 
-const center = { lat: 51, lng: 10 };
+export default {
+  name: "LeafletMap",
+  data() {
+    return {
+      map: null,
+    };
+  },
+  methods: {
+    parseGeo(data) {
+      return data.results.map((item) => {
+        return {
+          lat: item.scoutOrganisation.zipCode.lat,
+          lng: item.scoutOrganisation.zipCode.lon,
+          stamm: item.scoutOrganisation.displayName,
+          count: item.participantCount,
+        };
+      });
+    },
+  },
+  mounted() {
+    const route = useRoute();
+    const eventId = route.params.id;
 
-import { useRoute } from "vue-router";
-const route = useRoute();
-
-const eventLocationSummary = computed(() => {
-  return eventStore.eventLocationSummary;
-});
-
-const hierarchyMappingDetailed = computed(() => {
-  if (picked.value === "invited") {
-    return eventStore.hierarchyMappingDetailed;
-  } else {
-    return eventStore.registrationLocationsSummary;
-  }
-});
-
-function getColorByBund(group) {
-  if (group?.bund) {
-    switch (group?.bund) {
-      case "DPBM":
-        return "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
-      case "PB Boreas":
-        return "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
-      case "PBMV":
-        return "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
-      case "PBW":
-        return "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
-      case "PB-Nordlicht":
-        return "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
-      case "PSD":
-        return "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
-      case "PB-Horizonte":
-        return "http://maps.google.com/mapfiles/ms/icons/orange-dot.png";
-      case "BEP":
-        return "http://maps.google.com/mapfiles/ms/icons/orange-dot.png";
-      case "FPS":
-        return "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
-      case "Jomsburg":
-        return "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
-      case "DPBH":
-        return "http://maps.google.com/mapfiles/ms/icons/orange-dot.png";
-      default:
-        return "http://maps.google.com/mapfiles/ms/icons/pink-dot.png";
+    eventStore.fetchEventSummary(eventId).then((response) => {
+      const data = response.data;
+      this.map = L.map("mapContainer").setView([46.05, 11.05], 5);
+      L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map);
+      //use a mix of renderers
+      var customPane = this.map.createPane("customPane");
+      var canvasRenderer = L.canvas({ pane: "customPane" });
+      customPane.style.zIndex = 399; // put just behind the standard overlay pane which is at 400
+      const geoData = this.parseGeo(data);
+      geoData.forEach((element) => {
+        L.marker([element.lat, element.lng], {
+          renderer: canvasRenderer,
+        })
+          .bindPopup(
+            L.popup({ maxWidth: 500 }).setContent(
+              "<div><h3>" +
+                element.stamm +
+                "</h3><p>Teilnehmer: " +
+                element.count +
+                "</p></div>"
+            )
+          )
+          .addTo(this.map);
+      });
+    });
+  },
+  onBeforeUnmount() {
+    if (this.map) {
+      this.map.remove();
     }
-  } else {
-    return "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
-  }
-}
-
-function getMarkerOptions(group) {
-  if (group?.zipCode?.lat) {
-    return {
-      position: {
-        lat: parseFloat(group?.zipCode?.lat || 0),
-        lng: parseFloat(group?.zipCode?.lon || 0),
-      },
-      icon: getColorByBund(group),
-    };
-  }
-  if (group?.scoutOrganisation?.zipCode?.lat) {
-    return {
-      position: {
-        lat: parseFloat(group?.scoutOrganisation?.zipCode?.lat || 0),
-        lng: parseFloat(group?.scoutOrganisation?.zipCode?.lon || 0),
-      },
-      icon: getColorByBund(group),
-    };
-  }
-}
-
-const types = [
-  { id: "registered", title: "Angemeldet" },
-  { id: "invited", title: "Eingeladen" },
-];
-
-const picked = ref("registered");
-
-onMounted(() => {
-  const eventId = route.params.id;
-  Promise.all([
-    eventStore.getEventLocationSummary(eventId),
-    eventStore.getRegistrationLocationsSummary(eventId),
-    eventStore.getHierarchyMappingDetailed(),
-  ]);
-});
+  },
+};
 </script>
+
+<style scoped>
+#mapContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
