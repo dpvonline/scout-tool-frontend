2dbe08d71497c6eac649ba993011d2429c52f634
Philip <philiphagemann@icloud.com>
2024-05-13 10:12:06 +0200

    Improve travel statistics

--- src/modules/event/components/statistic/attribute/list/TravelItem.vue
+++ src/modules/event/components/statistic/attribute/list/TravelItem.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="flex min-w-0 flex-1 items-center">
-    <div class="flex-3">
+  <div class="flex min-w-0 flex-1 items-center space-x-4">
+    <div class="flex-1">
       {{ props.item.registration.scoutOrganisation.name }}
     </div>
-    <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-1 md:gap-4">
-      <div>
-        <p class="text-sm font-medium text-black-600">
-          {{ props.item.numberPersons }} Person/en mit
-          {{ props.item.typeField }} um
-          {{ $dayjs(props.item.dateTimeField).format("llll") }}
-          ->
-          {{ props.item.description }}
-        </p>
-      </div>
+    <div class="flex-1 text-sm font-medium text-black-600">
+      {{ props.item.numberPersons }} Person/en
     </div>
-    <div class="flex-0">
-      <PrimaryButton @click="onRegClicked(props?.item?.registration?.id)" color="blue"
-        >öffnen</PrimaryButton
-      >
+    <div class="flex-1 text-sm font-medium text-black-600">
+      {{ props.item.typeField }}
+    </div>
+    <div class="flex-1 text-sm font-medium text-black-600">
+      {{ $dayjs(props.item.dateTimeField).format("llll") }}
+    </div>
+    <div class="flex-1 text-sm font-medium text-black-600">
+      {{ props.item.description }}
+    </div>
+    <div class="flex-none">
+      <PrimaryButton @click="onRegClicked(props?.item?.registration?.id)" color="blue">
+        öffnen
+      </PrimaryButton>
     </div>
   </div>
 </template>
@@ -25,20 +26,13 @@
 <script setup lang="ts">
 import PrimaryButton from "@/components/button/Primary.vue";
 
-import {
-  ClipboardIcon,
-  RocketLaunchIcon,
-  CheckCircleIcon,
-  EnvelopeIcon,
-} from "@heroicons/vue/24/outline";
-
-import { useRoute, useRouter } from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 
 const router = useRouter();
 const route = useRoute();
 
+import {useEventRegisterStore} from "@/modules/event/store/register.ts";
 
-import { useEventRegisterStore } from "@/modules/event/store/register.ts";
 const eventRegisterStore = useEventRegisterStore();
 
 const props = defineProps({
